ed32f353e8e060a3fca824cd5c02782c74d778a1
ViethoangMai-dev <viethoangmai21199@.com>
2024-03-08 14:02:10 +0700

    Day 19

--- Day_19/assets/main.js
+++ Day_19/assets/main.js
@@ -1,2 +1,18 @@
 var paragraph = `Giờ mới biết làm gì có ai yêu mãi một người em ơi`;
-console.log(paragraph.split(" "));
+paragraph = paragraph.replaceAll(" ", `</span> <span>`);
+paragraph = `<span>${paragraph}</span>`;
+var i = 0;
+setInterval(function () {
+    var index = paragraph.charAt(i);
+    var nextIndex = paragraph.charAt(i + 1);
+    if (index === ">" && nextIndex !== " ") {
+        var newPara =
+            paragraph.slice(0, i) + ` class="highlight"` + paragraph.slice(i);
+        document.body.innerHTML = newPara;
+    }
+    i++;
+    if (i === paragraph.length) {
+        i = 0;
+    }
+}, 50);
+document.write(paragraph);
