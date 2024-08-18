ddf1d3f415f8e158687cec915e4a598a4658ddd4
ViethoangMai-dev <viethoangmai21199@.com>
2024-08-18 17:03:51 +0700

    update

--- mindmap-pj/src/middleware.js
+++ mindmap-pj/src/middleware.js
@@ -5,45 +5,51 @@ export const middleware = async (req) => {
     const path = req.nextUrl.pathname;
     const idMap = path.replace("/my-Mindmap", "").replace("/", "");
 
-    // Fetch toàn bộ dữ liệu từ JSON Server
-    const response = await fetch(`${process.env.NEXT_PUBLIC_URL_MINDMAP}`);
+    try {
+        const response = await fetch(`${process.env.NEXT_PUBLIC_URL_MINDMAP}`);
+        if (!response.ok) throw new Error("Failed to fetch data");
 
-    const data = await response.json();
+        const data = await response.json();
+        let mindMap;
+        let ownerId;
+        let status = "";
 
-    // Tìm Mind Map dựa trên idMap trong toàn bộ users
-    let mindMap;
-    let ownerId;
-    let status = "";
-    data.forEach((user) => {
-        const foundMap = user.mindMapData.find((map) => map.idMap === idMap);
-        if (foundMap) {
-            mindMap = foundMap;
-            ownerId = user.id;
-            status = mindMap.checkStatus;
-        }
-    });
+        data.forEach((user) => {
+            const foundMap = user.mindMapData.find(
+                (map) => map.idMap === idMap
+            );
+            if (foundMap) {
+                mindMap = foundMap;
+                ownerId = user.id;
+                status = mindMap.checkStatus;
+            }
+        });
 
-    if (status === "private") {
         const session = await getSession(req);
         const user = session?.user;
 
-        // Kiểm tra nếu người dùng không đăng nhập hoặc không phải là chủ sở hữu
-        if (!user) {
-            return NextResponse.redirect(new URL("/api/auth/login", req.url));
+        if (status === "private") {
+            if (!user) {
+                return NextResponse.redirect(
+                    new URL("/api/auth/login", req.url)
+                );
+            }
+            if (user.sub !== ownerId) {
+                return NextResponse.redirect(new URL("/notFound", req.url));
+            }
+        } else if (path.includes("/my-Mindmap")) {
+            if (!user) {
+                return NextResponse.redirect(
+                    new URL("/api/auth/login", req.url)
+                );
+            }
         }
-        if (user.sub !== ownerId) {
-            return NextResponse.redirect(new URL("/notFound", req.url));
-        }
-    } else if (path.includes("/my-Mindmap")) {
-        const session = await getSession(req);
-        const user = session?.user;
 
-        if (!user) {
-            return NextResponse.redirect(new URL("/api/auth/login", req.url));
-        }
+        return NextResponse.next();
+    } catch (error) {
+        console.error("Error in middleware:", error);
+        return NextResponse.redirect(new URL("/error", req.url));
     }
-
-    return NextResponse.next();
 };
 
 export const config = {
